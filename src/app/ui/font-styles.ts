e2d6dbac5f500cb060676ac9f11638ea49d07364
dsllt <dayanesallet@hotmail.com>
2024-01-13 14:40:38 -0300

    Creates a font definition file

---
+++ src/app/ui/font-styles.ts
@@ -0,0 +1,6 @@
+import { Inter, Roboto } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const roboto_400 = Roboto({ subsets: ['latin'], weight: '400' });
+export const roboto_700 = Roboto({ subsets: ['latin'], weight: '700' });
+
