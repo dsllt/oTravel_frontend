5cdf67c19a7aec53455dd2f7fc3c18dcf55eb19a
dsllt <dayanesallet@hotmail.com>
2024-01-13 14:39:20 -0300

    Creates a mock data file

---
+++ src/app/lib/coffee-data.js
@@ -0,0 +1,105 @@
+const users = [
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442a',
+    name: 'User',
+    email: 'user@nextmail.com',
+    password: '123456',
+  },
+];
+
+const coffees = [
+  {
+    id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
+    name: 'THE COFFEE',
+    description: '',
+    address: 'Rua Fernandes Vieira, 656 - Bom Fim',
+    phone: '',
+    rating: 4.5,
+    imageUrl:
+      'https://res.cloudinary.com/hbhhv9rz9/image/upload/f_auto,c_limit,w_3840,q_auto/cafes/Cafe%20Landing%20Page/Cafe-Jackson-Square-Hero.jpg',
+    slug: 'the-coffee',
+    latitude: 10.456,
+    longitude: 10.98,
+  },
+  {
+    id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
+    name: 'THE COFFEE',
+    description: '',
+    address: 'Rua Fernandes Vieira, 656 - Bom Fim',
+    phone: '',
+    rating: 4.5,
+    imageUrl:
+      'https://res.cloudinary.com/hbhhv9rz9/image/upload/f_auto,c_limit,w_3840,q_auto/cafes/Cafe%20Landing%20Page/Cafe-Jackson-Square-Hero.jpg',
+    slug: 'the-coffee',
+    latitude: 10.456,
+    longitude: 10.98,
+  },
+  {
+    id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
+    name: 'THE COFFEE',
+    description: '',
+    address: 'Rua Fernandes Vieira, 656 - Bom Fim',
+    phone: '',
+    rating: 4.5,
+    imageUrl:
+      'https://res.cloudinary.com/hbhhv9rz9/image/upload/f_auto,c_limit,w_3840,q_auto/cafes/Cafe%20Landing%20Page/Cafe-Jackson-Square-Hero.jpg',
+    slug: 'the-coffee',
+    latitude: 10.456,
+    longitude: 10.98,
+  },
+];
+
+
+
+const coffeeTypes = [
+  {
+    category: 'Puristas',
+    name: 'Pure Black',
+    type: 'Expresso',
+  },
+  {
+    category: 'Puristas',
+    name: 'Americano',
+    type: 'Expresso diluído',
+  },
+  {
+    category: 'Puristas',
+    name: 'True White',
+    type: 'Latte',
+  },
+  {
+    category: 'Autorais',
+    name: 'Mad Mocha',
+    type: 'Mocha',
+  },
+]
+
+const foodTypes = [
+  {
+    category: 'Doces',
+    name: 'Cakes',
+    type: 'Bolo',
+  },
+  {
+    category: 'Doces',
+    name: 'Cookies',
+    type: 'Cookie',
+  },
+  {
+    category: 'Bakery',
+    name: 'Croissant',
+    type: 'Croissant',
+  },
+  {
+    category: 'Bakery',
+    name: 'Pain au Chocolat',
+    type: 'Massa folhada',
+  },
+]
+
+module.exports = {
+  users,
+  coffees,
+  coffeeTypes,
+  foodTypes
+};
