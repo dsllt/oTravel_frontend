e4234e8af524b3efa1e987bc639c5d3f237bb8d2
dsllt <dayanesallet@hotmail.com>
2024-01-15 15:04:47 -0300

    Includes coffee-box component in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,24 @@ import styles from './page.module.css'
 import { fetchCoffees } from './lib/data'
 import { SearchInput } from './ui/home/search-input';
 import { SearchHeader } from './ui/home/search-header';
+import { CoffeeBox } from './ui/home/coffee-box';
 
 
 export default async function Home() {
   const coffeeData = await fetchCoffees();
+  const coffeeInfo = {
+    createdAt: "2024-01-13T15:56:56.376Z",
+    name: "Heathcote - Spinka",
+    image: "https://loremflickr.com/640/480/business",
+    description: "secondary",
+    address: "Kunde Views, 213/2",
+    phone: "1-392-289-9430",
+    latitude: "49.3316",
+    longitude: "-169.3190",
+    rating: 4.4,
+    slug: "fuga-et-veniam",
+    id: "1"
+  }
   return (
     <main className={styles.main}>
       <div className={styles.header}>
@@ -15,7 +29,11 @@ export default async function Home() {
 
       <div className={styles['body-container']}>
         <SearchHeader />
-
+        <div className={styles['coffees-container']}>
+          <CoffeeBox coffeeInfo={coffeeInfo} />
+          <CoffeeBox coffeeInfo={coffeeInfo} />
+          <CoffeeBox coffeeInfo={coffeeInfo} />
+        </div>
       </div>
     </main>
   )
--- src/app/ui/home/search-input.tsx
+++ src/app/ui/home/search-input.tsx
@@ -4,8 +4,8 @@ import styles from './search-input.module.css'
 export function SearchInput() {
   return(
     <div className={styles.main}>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none"  stroke-width="1.5" stroke="currentColor" className={styles.icon}>
-        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none"  strokeWidth="1.5" stroke="currentColor" className={styles.icon}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
       </svg>
       <input className={styles['search-input']} type="text" placeholder='Busque por um café' />
   </div>
