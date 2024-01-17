08780ca912188d0a9cecc23033d0184398fe4642
dsllt <dayanesallet@hotmail.com>
2024-01-17 20:14:48 -0300

    Simplifies map render

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,13 +1,10 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
+import {APIProvider, Map} from '@vis.gl/react-google-maps';
 import styles from "./page.module.css"
-import clsx from 'clsx';
-
-import { ReactNode, useEffect, useRef, useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Wrapper } from '@googlemaps/react-wrapper'
-import { CoffeeIcon } from "../ui/coffee-icon"
-import { StarIcon } from "../ui/star-icon"
+import { AdvancedMarker } from '@vis.gl/react-google-maps';
+import { CoffeeIcon } from '../ui/coffee-icon';
+import { useState } from 'react';
+import { StarIcon } from '../ui/star-icon';
 
 const mapOptions = {
   apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
@@ -16,6 +13,7 @@ const mapOptions = {
   zoom: 16,
   disableDefaultUI: true,
 }
+
 const coffeeInfo = [{
   createdAt: "2024-01-13T15:56:56.376Z",
   name:'THE COFFEE',
@@ -42,151 +40,52 @@ const coffeeInfo = [{
   slug: "brasco-cafe",
   id: "2"
 }]
-
-type SelectedCoffeeType =
-  | {
-      name: string
-      address: string
-      rating: number
-      slug: string
-      latitude: number
-      longitude: number
-    }
-  | undefined
-type MapType = google.maps.Map | undefined;
-type RefType = HTMLDivElement | null;
 type HoveredMarkerProps = string | null;
-type PositionProps = {
-    lat: number
-    lng: number
-  }
-type MapProps = {
-    zoom: number;
-    center: PositionProps;
-  };
-type ChildrenProps = React.ReactNode;
-type OnClickProps = (event: React.MouseEvent) => void;
-type MarkerProps = {
-    map: MapProps
-    children: ChildrenProps
-    position: PositionProps
-    onClick?: OnClickProps
-  }
-type RootRefType = {
-    render: (children: ReactNode) => void;
-    unmount: (children: ReactNode) => void;
-  } |null
 
-export default function Maps() {
+
+export default function Page() {
+  const [hoveredMarker, setHoveredMarker] = useState<HoveredMarkerProps>(null)
   return (
     <div className={styles.container}>
       <h2>Encontre sua próxima experiência</h2>
-      <div className={styles.map}>
-        <Wrapper
-          apiKey={mapOptions.apiKey}
-          version="beta"
-          libraries={['marker']}
-        >
-          <CoffeeMap />
-        </Wrapper>
-      </div>
-    </div>
-  )
-}
-
-function CoffeeMap() {
-  const [map, setMap] = useState<MapType>();
-  const ref = useRef<RefType>(null);
-
-  useEffect(() => {
-    if (ref.current){
-      setMap(new window.google.maps.Map(ref.current, mapOptions))
-    }
-  }, [])
-
-  return (
-    <>
-      <div ref={ref} className={styles.map} style={{height: '500px'}}/>
-      {map && <Coffees map={map} />}
-    </>
-  )
-}
-
-function Coffees({ map }: any) {
-  const [selectedCoffee, setSelectedCoffee] =
-    useState<SelectedCoffeeType>(undefined)
-  const [hoveredMarker, setHoveredMarker] = useState<HoveredMarkerProps>(null)
-
-  return (
-    <>
-      {Object.entries(coffeeInfo).map(([key, coffee]) => (
-        <Marker
-          map={map}
-          key={key}
-          position={{lat: parseFloat(coffee.latitude), lng: parseFloat(coffee.longitude)}}
-          onClick={() => {console.log("Clicked")}}
-        >
-          <div
-            className={styles.marker}
-            onMouseEnter={() => setHoveredMarker(coffee.slug)}
-            onMouseLeave={() => setHoveredMarker(null)}
+      <APIProvider apiKey={mapOptions.apiKey}>
+        <div className={styles.map}>
+          <Map
+            zoom={15}
+            className={styles.map}
+            center={mapOptions.center}
+            disableDefaultUI={mapOptions.disableDefaultUI}
+            mapId={mapOptions.mapId}
           >
-            <CoffeeIcon />
-            {hoveredMarker === coffee.slug ? (
-              <>
-                <div className={styles['marker-info']}>
-                  <p>{coffee.name}</p>
-                  <p>{coffee.address}</p>
+            {coffeeInfo.map(coffee => {
+              return(
+                <div key={coffee.id}
+                  onMouseEnter={() => setHoveredMarker(coffee.slug)}
+                  onMouseLeave={() => setHoveredMarker(null)}
+                >
+                  <AdvancedMarker key={coffee.id} className={styles.marker} position={{lat: parseFloat(coffee.latitude), lng: parseFloat(coffee.longitude)}} > 
+                    <CoffeeIcon />
+                    {hoveredMarker === coffee.slug ? (
+                      <>
+                        <div className={styles['marker-info']}>
+                          <p>{coffee.name}</p>
+                          <p>{coffee.address}</p>
+                        </div>
+                        <div className={styles['marker-rating']}>
+                          <p>{coffee.rating}</p>
+                          <StarIcon />
+                        </div>
+                      </>
+                    ) : (
+                      ''
+                    )}
+                  </AdvancedMarker>
                 </div>
-                <div className={styles['marker-rating']}>
-                  <p>{coffee.rating}</p>
-                  <StarIcon />
-                </div>
-              </>
-            ) : (
-              ''
-            )}
-          </div>
-        </Marker>
-      ))}
-    </>
-  )
+              )
+            })}
+          </Map>
+        </div>
+      </APIProvider>
+    </div>
+  );
 }
-
-function Marker({ map, position, children, onClick }: MarkerProps) {
-  const rootRef = useRef<RootRefType>(null);
-  const markerRef = useRef<google.maps.marker.AdvancedMarkerElement | undefined>();
-
-  useEffect(() => {
-    if (!rootRef.current) {
-      const container = document.createElement('div')
-      rootRef.current = createRoot(container)
-
-      markerRef.current = new google.maps.marker.AdvancedMarkerElement({
-        position,
-        content: container,
-      })
-    }
-
-    return () => {
-      if(markerRef.current){
-        markerRef.current.map = null
-      }
-    }
-  }, [])
-
-  useEffect(() => {
-    if(rootRef.current){
-      rootRef.current.render(children)
-    }
-    if(markerRef.current){
-      markerRef.current.position = position
-      markerRef.current.map = map
-      const listener = markerRef.current.addListener('click', onClick)
-      return () => listener.remove()
-    }
-  }, [map, position, children, onClick])
-
-  return null
-}
-
