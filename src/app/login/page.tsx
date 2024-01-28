f81c5b7ddfd6a323ec31d075dc256761bda5d3dc
Dayane Sallet <dayanesallet@hotmail.com>
2024-01-28 18:00:59 -0300

    Initializes login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image"
+import styles from "./page.module.css"
+
 export default function Page() {
-  return <p>Login Page</p>;
+  return(
+    <div className={styles.container}>
+      <div className={styles.image}></div>
+      
+      <div className={styles.text}>
+        <div className={styles.header}>
+          <h1>Boas vindas!</h1>
+          <span>Faça seu login ou acesse como visitante.</span>
+        </div>
+
+        <button>
+        <Image src="/google-logo.svg" alt="Google Logo"  width={50} height={50}/> Entrar com o Google
+        </button>
+        <button>
+        <Image src="/visitor-logo.svg" alt="Google Logo"  width={50} height={50}/> Entrar como visitante
+        </button>
+      </div>
+    </div>
+  )
 }
