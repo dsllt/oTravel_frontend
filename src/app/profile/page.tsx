678289487fe87e95a76fd401f4e2561ecfdb8a74
dsllt <dayanesallet@hotmail.com>
2024-01-28 12:41:13 -0300

    Creates profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,15 @@
+import ReviewBox from '@app/ui/profile/review-box';
+import styles from './page.module.css';
 export default function Page() {
-  return <p>Profile Page</p>;
+  return(
+  <div className={styles.container}>
+    <div className={styles.main}>
+      <div className={styles.reviews}>
+        <h3>Reviews</h3>
+        <ReviewBox />
+        <ReviewBox />
+      </div>
+    </div>
+  </div>
+    )
 }
