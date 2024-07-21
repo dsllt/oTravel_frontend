9d1436beee4c051e9da030db6db222bc53baa1cf
dsllt <dayanesallet@hotmail.com>
2024-07-21 18:45:38 -0300

    feat: creates favorites sidebar

---
+++ src/components/profile/favorite-place-box.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Info, Trash } from 'lucide-react';
+import { useRouter } from 'next/navigation'
+
+export function FavoritePlaceBox({ placeInfo }: any) {
+  const router = useRouter();
+  function handleClickPlaceBox(id: string) {
+    router.push(`/explore/${id}`)
+  }
+
+  return (
+    <div className="bg-zinc-900 rounded-lg shadow-xl hover:opacity-80 hover:cursor-pointer flex w-full min-h-18 max-h-18" >
+      <figure className="w-48 h-40 overflow-hidden rounded-tl-lg rounded-bl-lg">
+        <img src={placeInfo.image_url} alt={placeInfo.name} className="object-cover w-full h-full" />
+      </figure>
+      <div className="flex flex-col p-4 gap-2">
+        <h2 className="text-sm">
+          {placeInfo.name}
+        </h2>
+        <p className='text-xs'>{placeInfo.city}</p>
+        <div className="mt-auto">
+          <div className="btn btn-ghost" onClick={() => handleClickPlaceBox(placeInfo.id)}>
+            <Info className='size-5 text-blue-600' />
+            Informações
+          </div>
+          <div className="btn btn-ghost">
+            <Trash className='size-5 text-red-300' />
+            Remover
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
