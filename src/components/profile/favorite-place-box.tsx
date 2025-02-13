e58bbcf749bb265c6ad10e5ec06a2eb0eef83672
dsllt <dayanesallet@hotmail.com>
2025-02-13 18:52:28 -0300

    refactor: changes img tag for Image component

--- src/components/profile/favorite-place-box.tsx
+++ src/components/profile/favorite-place-box.tsx
@@ -2,17 +2,14 @@
 
 import { Info, Trash } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { useContext } from 'react';
-import { UserContext } from '../../context/userContext';
 import { Favorite } from '../../domain/models/favorite';
+import Image from 'next/image';
 
 type FavoritePlaceBoxProps = {
   placeInfo: Favorite;
 };
 
 export function FavoritePlaceBox({ placeInfo }: FavoritePlaceBoxProps) {
-  const { setFavorites, favorites } = useContext(UserContext);
-
   const router = useRouter();
   function handleClickPlaceBox(slug: string) {
     router.push(`/explore/${slug}`);
@@ -23,16 +20,15 @@ export function FavoritePlaceBox({ placeInfo }: FavoritePlaceBoxProps) {
       prevState.filter((favorite) => favorite.id !== id),
     );
   }
-
   return (
     <div className="bg-zinc-900 rounded-lg shadow-xl hover:opacity-80 hover:cursor-pointer flex w-full min-h-18 max-h-18">
-      <figure className="w-48 h-40 overflow-hidden rounded-tl-lg rounded-bl-lg">
-        <img
-          src={placeInfo.image_url}
-          alt={placeInfo.name}
-          className="object-cover w-full h-full"
-        />
-      </figure>
+      <Image
+        src={placeInfo.imageUrl}
+        alt={placeInfo.name}
+        width={100}
+        height={50}
+        className="w-48 h-48 overflow-hidden rounded-tl-lg rounded-bl-lg object-cover"
+      />
       <div className="flex flex-col p-4 gap-2">
         <h2 className="text-sm">{placeInfo.name}</h2>
         <p className="text-xs">{placeInfo.city}</p>
